4f161dca4af7721dca78b332405333e16ba9ace3
Joshua Trono <Josh.trono@gmail.com>
2023-03-31 22:53:57 -0400

    fixed authentication

--- app/controllers/reimbursement_controller.rb
+++ app/controllers/reimbursement_controller.rb
@@ -1,29 +1,21 @@
 require_relative '../../lib/jwt_session'
 class ReimbursementController < ApplicationController
+  before_action :add_auth_header_to_params
+  # before_action :authorization
   def create
     # need to add checks to see if all the null fields are filled
-    #@user = User.new()
 
-    #puts "PARAM #{params.class}"
-    puts "PARAM #{params}"
-    puts "AUTHORIZATION #{authorization}"
-    puts "CLASS #{authorization["user_id"].class}"
-    puts "AUTHORIZATION[USER_ID] #{authorization["user_id"]}"
-
-
-
-    params[:user_id] = authorization["user_id"]
-    puts "PARAM #{params}"
-    puts "class #{reimburse_params.class}"
-    puts "Rimburse_params #{reimburse_params}"
-
-    test2 = reimburse_params
+    # makes new reimbursement
     @reimbursement = Reimbursement.new(reimburse_params)
-    puts "REIMBURSEMENT #{@reimbursement}"
-    if authorization["user_id"].nil?
-      render status: 400, json: {message: "User not valid"}
+    # checks if the user_id in the reimbursement is the same as the user_id in the token
+    puts params[:authorization]
+    if !(@reimbursement.user_id = params[:authorization]["user_id"])
+      render status: 400, json: {message: "Invalid"}
+
+      # if the reimbursement is valid, save it and return a success message
     elsif @reimbursement.save
       render status: 200, json: {message: "Reimbursement request made successfully"}
+      # if the reimbursement is not valid, return an error message
     else
       render status: 400, json: {message: "Invalid"}
     end
@@ -44,14 +36,25 @@ class ReimbursementController < ApplicationController
   def update
   end
 
+
+  # returns the reimbursement params that are allowed to be passed in
   private
   def reimburse_params
-    params.require(:reimbursement).permit(:description, :amount, :status)
+    params.require(:reimbursement).permit(:description, :amount, :status, :user_id)
   end
 
-  def authorization
-    token = request.env["HTTP_AUTHORIZATION"].split(" ")
-    token = token[1]
-    jwt = Jwt_Session.decode(token)[0]
+  # adds the authorization header to the params
+  # this is used to get the user_id from the token
+  # this is used to check if the user_id in the reimbursement is the same as the user_id in the token
+  def add_auth_header_to_params
+    auth_header = request.headers["Authorization"].split(" ")
+    jwt = Jwt_Session.decode(auth_header[1])[0] if auth_header[0].present?
+    params[:authorization] = jwt
   end
+
+  # def authorization
+  #   token = request.env["HTTP_AUTHORIZATION"].split(" ")
+  #   token = token[1]
+  #   jwt = Jwt_Session.decode(token)[0]
+  # end
 end
