46fc029da1d1c737f7d4797c62b5f41d1cc57ce7
Joshua Trono <Josh.trono@gmail.com>
2023-04-10 17:22:24 -0400

    added testing coverage and 2 tests that covers 36% of the app folder.

--- test/models/sessions_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SessionsControllerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,15 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "simplecov"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
-
+  SimpleCov.start do
+    add_filter '/test/'
+    track_files 'app/**/*.rb'
+  end
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
