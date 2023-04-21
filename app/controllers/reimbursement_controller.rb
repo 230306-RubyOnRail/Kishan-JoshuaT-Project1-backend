3b9819b3ecdea1e21cb90cf5d95e84bb2730bddc
Joshua Trono <Josh.trono@gmail.com>
2023-04-20 20:49:45 -0400

    change to try to fix a bug

--- app/controllers/reimbursement_controller.rb
+++ app/controllers/reimbursement_controller.rb
@@ -94,9 +94,12 @@ class ReimbursementController < ApplicationController
   # this is used to update a reimbursement
   # this is used to check if the user_id in the reimbursement is the same as the user_id in the token
   def reimburse_params
-    if params[:authorization][0][:account_type] != "manager"
+    if params[:authorization][0][:account_type] == "manager"
+      params[:reimbursement][:user_id] = params[:reimbursement][:user_id]
+    else
       params[:reimbursement][:user_id] = params[:authorization][0][:user_id]
     end
+
     params.require(:reimbursement).permit(:description, :amount, :status, :user_id)
   end
 
